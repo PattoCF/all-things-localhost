edf9fcf199e90dcc590d2af1786c78a3ec0043a2
Patrick Geers <59477906+PattoCF@users.noreply.github.com>
2023-09-08 08:13:06 +0200

    Initial commit

---
+++ src/locations/Field.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
+import JsonObject from "../field_types/jsonObject";
+import ShortText from "../field_types/shortText";
+import ShortTextList from "../field_types/shortTextList";
+import LongText from "../field_types/longText";
+import RichtText from "../field_types/richtText";
+import NumberDecimal from "../field_types/numberDecimal";
+import NumberInteger from "../field_types/numberInteger";
+import DateTime from "../field_types/dateTime";
+import Location from "../field_types/location";
+import Boolean from "../field_types/boolean";
+import EntryReference from "../field_types/entryReference";
+import EntryReferenceMany from "../field_types/entryReferenceMany";
+import AssetReference from "../field_types/assetReference";
+import AssetReferenceMany from "../field_types/assetReferenceMany";
+
+const Field = () => {
+  const sdk = useSDK();
+  /*
+     To use the cma, inject it as follows.
+     If it is not needed, you can remove the next line.
+  */
+  // const cma = useCMA();
+  // If you only want to extend Contentful's default editing experience
+  // reuse Contentful's editor components
+  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
+  debugger;
+
+  let returnObject = undefined;
+
+  switch (sdk.field.type) {
+    case "Object":
+      returnObject = <JsonObject sdk={sdk}></JsonObject>;
+      break;
+    case "Symbol":
+      returnObject = <ShortText sdk={sdk}></ShortText>;
+      break;
+    case "Array":
+      switch (sdk.field.items.type) {
+        case "Symbol":
+          returnObject = <ShortTextList sdk={sdk}></ShortTextList>;
+          break;
+        case "Link":
+          switch (sdk.field.items.linkType) {
+            case "Entry":
+              returnObject = (
+                <EntryReferenceMany sdk={sdk}></EntryReferenceMany>
+              );
+              break;
+
+            case "Asset":
+              returnObject = (
+                <AssetReferenceMany sdk={sdk}></AssetReferenceMany>
+              );
+              break;
+            default:
+              break;
+          }
+
+          break;
+        default:
+          break;
+      }
+      break;
+
+    case "Text":
+      returnObject = <LongText sdk={sdk}></LongText>;
+      break;
+    case "RichText":
+      returnObject = <RichtText sdk={sdk}></RichtText>;
+      break;
+    case "Number":
+      returnObject = <NumberDecimal sdk={sdk}></NumberDecimal>;
+      break;
+    case "Integer":
+      returnObject = <NumberInteger sdk={sdk}></NumberInteger>;
+      break;
+    case "Date":
+      returnObject = <DateTime sdk={sdk}></DateTime>;
+      break;
+    case "Location":
+      returnObject = <Location sdk={sdk}></Location>;
+      break;
+    case "Boolean":
+      returnObject = <Boolean sdk={sdk}></Boolean>;
+      break;
+    case "Link":
+      switch (sdk.field.linkType) {
+        case "Entry":
+          returnObject = <EntryReference sdk={sdk}></EntryReference>;
+          break;
+        case "Asset":
+          returnObject = <AssetReference sdk={sdk}></AssetReference>;
+          break;
+        default:
+          break;
+      }
+      break;
+
+    default:
+      break;
+  }
+
+  return <div>{returnObject}</div>;
+};
+
+export default Field;
